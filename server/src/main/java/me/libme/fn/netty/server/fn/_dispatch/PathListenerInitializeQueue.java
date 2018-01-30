cbb1b8f7477b694db6c75c021ba7e84470c7ff95
J <3cgg@163.com>
2018-01-30 20:26:05 +0800

    -

--- server/src/main/java/me/libme/fn/netty/server/fn/_dispatch/PathListenerInitializeQueue.java
+++ server/src/main/java/me/libme/fn/netty/server/fn/_dispatch/PathListenerInitializeQueue.java
@@ -1,8 +1,9 @@
 package me.libme.fn.netty.server.fn._dispatch;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Queue;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
@@ -10,27 +11,37 @@ import java.util.concurrent.LinkedBlockingQueue;
  */
 public class PathListenerInitializeQueue {
 
-    private final Executor executor= Executors.newFixedThreadPool(1);
 
-    private final Queue<Runnable> queue=new LinkedBlockingQueue<>();
+    private static final Logger LOGGER = LoggerFactory.getLogger(PathListenerInitializeQueue.class);
 
-    private static final PathListenerInitializeQueue instance=new PathListenerInitializeQueue();
+//    private final ExecutorService executor= Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    private final Queue<Runnable> queue = new LinkedBlockingQueue<>();
+
+    private static final PathListenerInitializeQueue instance = new PathListenerInitializeQueue();
 
     private PathListenerInitializeQueue() {
     }
 
-    public static PathListenerInitializeQueue get(){return instance;}
+    public static PathListenerInitializeQueue get() {
+        return instance;
+    }
 
 
-    public void offer(Runnable runnable){
+    public void offer(Runnable runnable) {
         queue.offer(runnable);
     }
 
 
-    public void allInitialize(){
+    public void allInitialize() {
         Runnable current;
-        while ((current=queue.poll())!=null){
-            executor.execute(current);
+        while ((current = queue.poll()) != null) {
+            try {
+                current.run();
+            } catch (Throwable e) {
+                LOGGER.error(e.getMessage(), e);
+                System.exit(-1);
+            }
         }
     }
 
